66bd8e9fa2c931942ac70b212123e1bece518b51
Mrietunjoy Sarkar <sarkarmrietunjoy@gmail.com>
2019-07-08 02:23:39 +0530

    removed the commented lines

--- app/src/main/java/com/example/pictureselect/MainActivity.java
+++ app/src/main/java/com/example/pictureselect/MainActivity.java
@@ -23,15 +23,9 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //pselect.Show(MainActivity.this);
-//                pic_select_from_storage.imgSelect(R.id.img, MainActivity.this);
-//                    pic_select.caller(R.id.img, MainActivity.this);
-                //pSelect.imgSelect(R.id.img, MainActivity.this);
+
                 picSelect.loadInto(MainActivity.this, img);
-//                Intent i=new Intent(MainActivity.this, pic_select_from_storage.class);
-//                startActivity(i);
-                //img.setImageBitmap(pic_select_from_storage.loadImg());
-                // pSelect.show(MainActivity.this);
+
             }
         });
     }
